cd8c73bff37bad69591668b52da97fb237d79ef5
Jens Rauch <jens.rauch@codenic.de>
2022-02-17 17:07:12 +0100

    fixed missing variable

--- src/amtronwallbox/client.py
+++ src/amtronwallbox/client.py
@@ -141,7 +141,7 @@ class Client(object):
 
             # This solves the string swap bytes issue
             # https://stackoverflow.com/questions/62411062/python-modbus-string-decoding-issue
-            for i in range(11):
+            for i in range(register):
                 rr.registers[i] = struct.unpack("<H", struct.pack(">H", rr.registers[i]))[0]
 
             if not rr.isError():
